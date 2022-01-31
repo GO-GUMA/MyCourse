d566d413640c5be1ff24d7a238996b4c359aefe3
GO-GUMA <gangsu1813@naver.com>
2022-01-31 19:09:59 +0900

    Add english video status

--- Chrome/video.js
+++ Chrome/video.js
@@ -1,4 +1,5 @@
 var closeVideoAutoBoolean = false; // Boolean => is user checked '영상 자동종료' 
+var langaugeSet = ['[재생준비] ','[일시정지] ','[재생중] ','[재생완료] '];
 
 // Get data from sync storage
 chrome.storage.sync.get('closeVideoAuto', function (result) {
@@ -10,20 +11,36 @@ chrome.storage.sync.get('closeVideoAuto', function (result) {
     // console.log('Auto close is ' + closeVideoAutoBoolean);
 });
 
+// Get language check status data from storage
+chrome.storage.sync.get('languageCheck', function (result) {
+    if (typeof result.languageCheck === "undefined") { // if Extension is running first time
+        chrome.storage.sync.set({ languageCheck: 'ko' }, function () {
+            console.log('[Init setting] Language setted ko');
+        });
+    }
+
+    if (result.languageCheck == 'en') {
+        langaugeSet = ['[Ready] ','[Paused] ','[Playing] ','[Complete] '];
+    } else {
+        langaugeSet = ['[재생준비] ','[일시정지] ','[재생중] ','[재생완료] '];
+    }
+    document.title = langaugeSet[0] + basedTitle; // Add video title to '[재생준비]'
+});
+
 function videoTitle() { // function videoTitle() => change video title
     var basedTitle = document.title; // Get current video title
-    document.title = '[재생준비] ' + basedTitle; // Add video title to '[재생준비]'
+    document.title = langaugeSet[0] + basedTitle; // Add video title to '[재생준비]' or '[Ready]'
 
     var target = document.getElementById('vod_player'); // Set target to Observing video status
 
     var observer = new MutationObserver(mutations => { // create new MutationObserver object
         mutations.forEach(() => {
             if (target.classList[2] == 'jw-state-paused') { // If video is paused
-                document.title = '[일시정지] ' + basedTitle;
+                document.title = langaugeSet[1] + basedTitle; // Cahnge video title to '[일시정지]' or '[Paused]'
             } else if (target.classList[2] == 'jw-state-playing') { // If video is playing
-                document.title = '[재생중] ' + basedTitle;
+                document.title = langaugeSet[2] + basedTitle; // Cahnge video title to '[재생중]' or '[Playing]'
             } else if (target.classList[2] == 'jw-state-complete') { // If video is done
-                document.title = '[재생완료] ' + basedTitle;
+                document.title = langaugeSet[3] + basedTitle; // Cahnge video title to '[재생완료]' or '[Complete]'
                 observer.disconnect(); // Close observer
 
                 if(closeVideoAutoBoolean) { // If user doesn't want auto close
